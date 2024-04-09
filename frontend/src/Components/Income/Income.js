354ff28dd60ff912134f4ae7f0165ac3b5b99fcd
prathameshpatil455 <132359689+prathameshpatil455@users.noreply.github.com>
2024-04-09 20:56:34 +0530

    frontend compoents and navigation bar

---
+++ frontend/src/Components/Income/Income.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../../styles/Layouts";
+
+
+function Income() {
+    return (
+        <IncomeStyled>
+            <InnerLayout>
+            </InnerLayout>
+        </IncomeStyled>
+    )
+}
+
+const IncomeStyled = styled.div`
+
+`;
+
+export default Income;
